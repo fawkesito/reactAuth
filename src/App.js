4e07dee06e3e47c8cb7e5f538632a37e65b036fe
fawkesito <snakemartinez10@gmail.com>
2022-07-08 18:38:05 -0500

    Finished WITHOUT AUTH

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import Login from "./Login";
+import Register from "./Register";
+
+export default function Account() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row>
+      {/* Register */}
+      <Col xs={12} sm={12} md={6} lg={6}>
+        <Register />
+      </Col>
+
+      {/* Login */}
+      <Col xs={12} sm={12} md={6} lg={6}>
+        <Login />
+      </Col>
+    </Row>
   );
 }
-
-export default App;
